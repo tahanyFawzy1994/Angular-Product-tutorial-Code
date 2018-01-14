49b15f46ec71ef4086ae227bb75d85f78604c3cd
tahany fawzy <tahany.fawzy@asset.com.eg>
2018-01-14 11:28:21 +0200

    Using a Component as a Directive (use product component in the app component)

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template : `<pm-products></pm-products>`
 })
 export class AppComponent {
   title = 'Angular: Getting Started';
