575a8c7a59d6df689d7e8ddeb118a86fac692442
tahany fawzy <tahany.fawzy@asset.com.eg>
2018-01-14 13:09:06 +0200

    product interface instead of any

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector : 'pm-products',
@@ -9,7 +10,7 @@ export class ProductListComponent{
     imageSize : number = 50;
     showImage : boolean = false;
     listFilter : string ='cart';
-    products : any[] = [
+    products : IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -39,26 +40,6 @@ export class ProductListComponent{
             "price": 8.9,
             "starRating": 4.8,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        },
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-        },
-        {
-            "productId": 10,
-            "productName": "Video Game Controller",
-            "productCode": "GMG-0042",
-            "releaseDate": "October 15, 2015",
-            "description": "Standard two-button video game controller",
-            "price": 35.95,
-            "starRating": 4.6,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
         }
     ];
     toggleImage() : void{
