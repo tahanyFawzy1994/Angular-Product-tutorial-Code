be638e67f0f0b0edd15cca588f4d30d5f4fb7756
tahany fawzy <tahany.fawzy@asset.com.eg>
2018-01-15 13:26:11 +0200

    use the service

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
-  template : `<pm-products></pm-products>`
+  template : `<pm-products></pm-products>`,
+  providers : [ProductService]
 })
 export class AppComponent {
   title = 'Angular: Getting Started';
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { IProduct } from "./product";
 import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
+import { ProductService } from "./product.service";
 
 @Component({
     selector : 'pm-products',
@@ -13,43 +14,17 @@ export class ProductListComponent implements OnInit{
     imageSize : number = 50;
     showImage : boolean = false;
     listFilter : string ='a';
-    products : IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+
+    //need to inject service
+    constructor(private productServiceObj : ProductService){
+    }
+    products : IProduct[];
     toggleImage() : void{
         this.showImage = !this.showImage;
     }
     ngOnInit() : void {
-        console.log("Init");
+        //will fill the array in initialization
+        this.products = this.productServiceObj.getProducts();
     }
     onRatingClicked(msg :string){
         this.pageTitle = 'PageList : '+msg;
